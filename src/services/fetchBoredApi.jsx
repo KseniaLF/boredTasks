8f6af1d79af1eeacaa5922551e95ae71846fba80
KseniaLF <llf9391@gmail.com>
2023-06-07 22:13:03 +0300

    fix query:  from http to https

--- src/services/fetchBoredApi.jsx
+++ src/services/fetchBoredApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getTask = async () => {
-  const response = await axios.get(`http://www.boredapi.com/api/activity/`);
+  const response = await axios.get(`https://www.boredapi.com/api/activity/`);
   return response.data;
 };
 
@@ -9,7 +9,7 @@ const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
 
 export const getImg = async (title) => {
   const response = await axios.get(
-    `http://api.unsplash.com/photos/random?query=${title}&client_id=${UNSPLASH_KEY}`
+    `https://api.unsplash.com/photos/random?query=${title}&client_id=${UNSPLASH_KEY}`
   );
   return response.data;
 };
