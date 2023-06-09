20aaecab5a8ac28bc46a1f189202b1b3086d2642
KseniaLF <llf9391@gmail.com>
2023-06-09 14:41:56 +0300

    add hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CssBaseline />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
